a4f800de845fb50df6cd78b4b569403fbd2aa8c8
Sukumarsawant <sawantsukumar@gmail.com>
2026-02-06 10:33:08 +0530

    6th feb W05

--- W_05.py
+++ W_05.py
@@ -3,55 +3,75 @@
 
 inventory = {}
 def add_item():
- item_id = input("Enter item ID: ")
- item_name = input("Enter item name : ")
- quantity = input("Enter quantity : ")
- price = input("Enter price : ")
- inventory[item_id]
+    item_id = input("Enter item ID: ")
+    inventory[item_id] = {}
+    inventory[item_id][0] = input("Enter item name : ")
+    inventory[item_id][1] = int(input("Enter quantity : "))
+    inventory[item_id][2] = float(input("Enter price : "))
+    item_name = inventory[item_id][0]
+    print(f"item '{item_name}' added successfully !!!")
 
- # Add item_id, name, quantity, price to inventory
- pass
 def update_quantity():
- # Update quantity of an existing item
- pass
+    item_id = input("Enter the item ID : ")
+    new_quantity = int(input("Enter the updated quantity"))
+    inventory[item_id][1] = new_quantity
 
 def search_item():
- # Search item using item_id and display details
- pass
+    item_id = input("Enter the item id : ")
+    if item_id in inventory:
+        print(f"Item found : '{inventory[item_id]}'")
+    else:
+        print("Item not found!!!")
+
+
 def display_items():
- # Display all items in inventory
- pass
+    print("-" * 53) 
+    print(f"| {"ID":^5} | {"Name":^20} | {"qty":>5} | {"price":>10} | ")
+    print("-" * 53)
+    for item_id,v in inventory.items():
+        print(f"| {item_id:^5} | {v[0]:<20} | {v[1]:>5} | {v[2]:>10} | ")
+    print("-" * 53)
+
 def total_inventory_value():
- # Calculate and display total inventory value
- pass
+    t_val = 0
+    for item_id,v in inventory.items():
+       t_val += v[2]
+    print(f"Total inventory value : {t_val}")
+ 
 def low_stock_items():
- # Display items with quantity less than 5
- pass
+    ct =0
+    print("Following are the low stack item : ")
+    for item_id,v in inventory.items():
+        if v[1]<5 : 
+            print(f"{v[0] + '\n'}")
+            ct  = ct+1
+    if ct == 0 : print("No low stock item ")
+
 # -------- MENU DRIVEN PROGRAM --------
 while True:
- print("\nInventory Management System")
- print("1. Add Item")
- print("2. Update Quantity")
- print("3. Search by Item ID")
- print("4. Display All Items")
- print("5. Total Inventory Value")
- print("6. Low Stock Items")
- print("7. Exit")
- choice = input("Enter your choice: ")
- if choice == "1":
- add_item()
- elif choice == "2":
- update_quantity()
- elif choice == "3":
- search_item()
- elif choice == "4":
- display_items()
- elif choice == "5":
- total_inventory_value()
- elif choice == "6":
- low_stock_items()
- elif choice == "7":
- print("Exiting system.")
- break
- else:
- print("Invalid choice. Try again.")
+    print("\nInventory Management System")
+    print("1. Add Item")
+    print("2. Update Quantity")
+    print("3. Search by Item ID")
+    print("4. Display All Items")
+    print("5. Total Inventory Value")
+    print("6. Low Stock Items")
+    print("7. Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        add_item()
+    elif choice == "2":
+        update_quantity()
+    elif choice == "3":
+        search_item()
+    elif choice == "4":
+        display_items()
+    elif choice == "5":
+        total_inventory_value()
+    elif choice == "6":
+        low_stock_items()
+    elif choice == "7":
+        print("Exiting system.")
+        break
+    else:
+        print("Invalid choice. Try again.")
